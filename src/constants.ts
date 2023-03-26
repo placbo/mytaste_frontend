3729559fc50559e092a734a52b50b44783fb9d09
PCB <perbjester@gmail.com>
2023-03-26 21:28:47 +0200

    xxx

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const USE_MOCK_DATA = import.meta.env.VITE_USE_MOCK === 'true';
 
-export const BASE_URL = import.meta.env.VITE_MYTASTE_API_HOST;
-export const ITEMS_URL = BASE_URL + '/api/items';
+export const BASE_URL = import.meta.env.VITE_MYTASTE_API_HOST ?? '';
+export const ITEMS_URL = BASE_URL + '/items';
