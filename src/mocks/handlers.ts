bb046397bbb156b10f7115f3f0a0e20ac8329e18
PCB <perbjester@gmail.com>
2024-08-31 11:17:51 +0200

    xxx

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,46 +4,46 @@ import { removeMockItemByIdFromItemList } from './mockHelpers';
 import { mockItems, mockReviews, mockTags } from './mocks';
 
 export const handlers = [
-  http.get(BASE_URL + '/items', async () => {
+  http.get(API_BASE_URL + '/items', async () => {
     await delay();
     return HttpResponse.json(mockItems);
   }),
 
-  http.get(BASE_URL + '/items/(\\d+)', async ({ request }) => {
+  http.get(API_BASE_URL + '/items/(\\d+)', async ({ request }) => {
     const itemid = request.url.split('/').pop();
     await delay();
     if (itemid === '12') return new HttpResponse(null, { status: 404 });
     return HttpResponse.json(mockItems.items[0]);
   }),
 
-  http.delete(BASE_URL + '/items/(\\d+)', ({ request }) => {
+  http.delete(API_BASE_URL + '/items/(\\d+)', ({ request }) => {
     const itemid = request.url.split('/').pop();
     mockItems.items = removeMockItemByIdFromItemList(mockItems.items, itemid);
 
     return HttpResponse.json('OK');
   }),
 
-  http.post(BASE_URL + '/items', async (request) => {
+  http.post(API_BASE_URL + '/items', async (request) => {
     console.log('XXX', request.params);
     await delay();
     return HttpResponse.json('OK', { status: 201 });
   }),
 
-  http.put(BASE_URL + '/items/(\\d+)', () => {
+  http.put(API_BASE_URL + '/items/(\\d+)', () => {
     return HttpResponse.json({});
   }),
 
-  http.get(BASE_URL + '/items/(\\d+)/tags', async () => {
+  http.get(API_BASE_URL + '/items/(\\d+)/tags', async () => {
     await delay();
     return HttpResponse.json(mockTags);
   }),
 
-  http.get(BASE_URL + '/items/(\\d+)/reviews', async () => {
+  http.get(API_BASE_URL + '/items/(\\d+)/reviews', async () => {
     await delay();
     return HttpResponse.json(mockReviews);
   }),
 
-  http.post(BASE_URL + '/auth/login', () => {
+  http.post(API_BASE_URL + '/auth/login', () => {
     return HttpResponse.json({ success: true, token: 'QXJuZSBCZW5vbmkgc3Bpc2VyIGJhbmFuCg==' });
   }),
 ];
