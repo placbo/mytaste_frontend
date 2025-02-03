1047759be0c46d20336e6a73b8e96a781d0301e4
PCB <perbjester@gmail.com>
2025-02-03 22:16:35 +0100

    fix: remove trailing slash from IMAGES_URL constant

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const API_BASE_URL = import.meta.env.VITE_MYTASTE_API_HOST ?? '';
 export const ITEMS_URL = API_BASE_URL + '/items';
 export const LOGIN_URL = API_BASE_URL + '/auth/login';
 
-export const IMAGES_URL = API_BASE_URL + '/images/';
+export const IMAGES_URL = API_BASE_URL + '/images';
 export const THUMBNAIL_URL = IMAGES_URL + '/thumbs/thumbnail.';
 export const IMAGE_UPLOAD_URL = API_BASE_URL + '/imageupload';
 
