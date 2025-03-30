ae845827514db22f994cf9556602ca8f265372ce
Bjelke, Per Christian <perchristian.bjelke@skatteetaten.no>
2025-03-30 23:50:04 +0200

    main: moved prettier

--- src/mocks/mockHelpers.ts
+++ src/mocks/mockHelpers.ts
@@ -1,7 +1,7 @@
 import { Item } from '../types';
 
 export function removeMockItemByIdFromItemList(items: Item[], itemId: string | undefined) {
-  for (var i = items.length - 1; i >= 0; --i) {
+  for (let i = items.length - 1; i >= 0; --i) {
     if (items[i].itemId == itemId) {
       items.splice(i, 1);
     }
